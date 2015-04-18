e51a03ed5f1f7c6627e9e09f5a5257a79a94c438
Coop <coop.coding@gmail.com>
2015-04-18 19:36:50 +1000

    Post 2015-04-18 19:36:42

--- js/prism.js
+++ js/prism.js
@@ -1207,7 +1207,7 @@ Prism.languages.yaml = {
 Prism.hooks.add('after-highlight', function (env) {
 	// works only for <code> wrapped inside <pre data-line-numbers> (not inline)
 	var pre = env.element.parentNode;
-	if (!pre || !/pre/i.test(pre.nodeName) || pre.className.indexOf('line-numbers') === -1) {
+	if (!pre || !/pre/i.test(pre.nodeName)) {
 		return;
 	}
 
